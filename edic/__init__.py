18aeb79158071f2b1ffe849b035a28e7f3be5061
Noah Fahlgren <noahfahlgren@gmail.com>
2023-08-27 20:40:38 -0500

    Rename filename to uri

--- edic/__init__.py
+++ edic/__init__.py
@@ -6,16 +6,16 @@ class Image(np.ndarray):
     """Generic image class that extends the np.ndarray class."""
 
     # From NumPy documentation
-    # Add filename attribute
-    def __new__(cls, input_array: np.ndarray, filename: str):
+    # Add uri attribute
+    def __new__(cls, input_array: np.ndarray, uri: str):
         obj = np.asarray(input_array).view(cls)
-        # New attribute filename stores the path and filename of the source file
-        obj.filename = filename
+        # New attribute uri stores uniform resource identifier of the source file
+        obj.uri = uri
         return obj
 
     def __array_finalize__(self, obj):
         if obj is not None:
-            self.filename = getattr(obj, "filename", None)
+            self.filename = getattr(obj, "uri", None)
 
     def __getitem__(self, key):
         # Enhance the np.ndarray __getitem__ method
@@ -27,35 +27,35 @@ class Image(np.ndarray):
 class BGR(Image):
     """Subclass of Image for Blue, Green, Red (BGR) images."""
 
-    def __new__(cls, input_array: np.ndarray, filename: str):
-        return Image.__new__(cls, input_array, filename)
+    def __new__(cls, input_array: np.ndarray, uri: str):
+        return Image.__new__(cls, input_array, uri)
 
     def __getitem__(self, key):
         # Overwrite the __getitem__ method to return a GRAY object if the
         # requested slice is 2D
         new_arr = super(Image, self).__getitem__(key)
         if len(new_arr.shape) == 2:
-            return GRAY(input_array=new_arr, filename=self.filename)
+            return GRAY(input_array=new_arr, uri=self.uri)
         return new_arr
 
 
 class RGB(Image):
     """Subclass of Image for Red, Green, Blue (RGB) images."""
 
-    def __new__(cls, input_array: np.ndarray, filename: str):
-        return Image.__new__(cls, input_array, filename)
+    def __new__(cls, input_array: np.ndarray, uri: str):
+        return Image.__new__(cls, input_array, uri)
 
     def __getitem__(self, key):
         # Overwrite the __getitem__ method to return a GRAY object if the
         # requested slice is 2D
         new_arr = super(Image, self).__getitem__(key)
         if len(new_arr.shape) == 2:
-            return GRAY(input_array=new_arr, filename=self.filename)
+            return GRAY(input_array=new_arr, uri=self.uri)
         return new_arr
 
 
 class GRAY(Image):
     """Subclass of Image for grayscale images."""
 
-    def __new__(cls, input_array: np.ndarray, filename: str):
-        return Image.__new__(cls, input_array, filename)
+    def __new__(cls, input_array: np.ndarray, uri: str):
+        return Image.__new__(cls, input_array, uri)
--- tests/test_classes.py
+++ tests/test_classes.py
@@ -4,61 +4,61 @@ from edic import Image, BGR, RGB, GRAY
 
 def test_image():
     """Test creating an Image class image."""
-    img = Image(input_array=np.zeros((10, 10), dtype=np.uint8), filename="image.png")
+    img = Image(input_array=np.zeros((10, 10), dtype=np.uint8), uri="image.png")
     assert isinstance(img, Image)
 
 
 def test_image_none():
     """Test creating an Image class image."""
-    img = Image(input_array=None, filename=None)
+    img = Image(input_array=None, uri=None)
     assert isinstance(img, Image)
 
 
 def test_image_slice():
     """Test subsetting an Image."""
-    img = Image(input_array=np.zeros((10, 10), dtype=np.uint8), filename="image.png")
+    img = Image(input_array=np.zeros((10, 10), dtype=np.uint8), uri="image.png")
     assert img[0:5, 0:5].shape == (5, 5)
 
 
 def test_bgr():
     """Test creating a BGR class image."""
-    bgr = BGR(input_array=np.zeros((10, 10, 3), dtype=np.uint8), filename="bgr.png")
+    bgr = BGR(input_array=np.zeros((10, 10, 3), dtype=np.uint8), uri="bgr.png")
     assert isinstance(bgr, BGR)
 
 
 def test_bgr_slice():
     """Test subsetting a BGR image."""
-    bgr = BGR(input_array=np.zeros((10, 10, 3), dtype=np.uint8), filename="bgr.png")
+    bgr = BGR(input_array=np.zeros((10, 10, 3), dtype=np.uint8), uri="bgr.png")
     assert bgr[0:5, 0:5].shape == (5, 5, 3)
 
 
 def test_rgb():
     """Test creating a RGB class image."""
-    bgr = RGB(input_array=np.zeros((10, 10, 3), dtype=np.uint8), filename="rgb.png")
+    bgr = RGB(input_array=np.zeros((10, 10, 3), dtype=np.uint8), uri="rgb.png")
     assert isinstance(bgr, RGB)
 
 
 def test_rgb_slice():
     """Test subsetting an RGB image."""
-    rgb = RGB(input_array=np.zeros((10, 10, 3), dtype=np.uint8), filename="rgb.png")
+    rgb = RGB(input_array=np.zeros((10, 10, 3), dtype=np.uint8), uri="rgb.png")
     assert rgb[0:5, 0:5].shape == (5, 5, 3)
 
 
 def test_gray():
     """Test creating a GRAY class image."""
-    gray = GRAY(input_array=np.zeros((10, 10), dtype=np.uint8), filename="gray.png")
+    gray = GRAY(input_array=np.zeros((10, 10), dtype=np.uint8), uri="gray.png")
     assert isinstance(gray, GRAY)
 
 
 def test_bgr_to_gray():
     """Test converting a BGR image to a GRAY image."""
-    bgr = BGR(input_array=np.zeros((10, 10, 3), dtype=np.uint8), filename="bgr.png")
+    bgr = BGR(input_array=np.zeros((10, 10, 3), dtype=np.uint8), uri="bgr.png")
     gray = bgr[:, :, 0]
     assert isinstance(gray, GRAY)
 
 
 def test_rgb_to_gray():
     """Test converting a RGB image to a GRAY image."""
-    rgb = RGB(input_array=np.zeros((10, 10, 3), dtype=np.uint8), filename="rgb.png")
+    rgb = RGB(input_array=np.zeros((10, 10, 3), dtype=np.uint8), uri="rgb.png")
     gray = rgb[:, :, 0]
     assert isinstance(gray, GRAY)
